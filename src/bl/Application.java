0719acadaf8f95518149ab0c853985a79c4bc170
powerex <flaxazbest@gmail.com>
2020-09-18 11:55:17 +0300

    Add one more

--- src/bl/Application.java
+++ src/bl/Application.java
@@ -3,5 +3,6 @@ package bl;
 public class Application {
     public static void main(String[] args) {
         System.out.println("From lab01");
+        System.out.println("From lab01 again");
     }
 }
